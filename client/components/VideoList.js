ae0e8f05dfcdfa274498623c45f3f2e0f068d2e0
David Ogor <doctown@yahoo.com>
2016-03-18 13:55:09 -0700

    Change VideoListEntry and VideoList to use class syntax

--- client/components/VideoList.js
+++ client/components/VideoList.js
@@ -1,9 +1,8 @@
 var VideoList = (props) => (
   <div className="video-list media">
-    <VideoListEntry video={props.videos[0]}/>
-    <VideoListEntry video={props.videos[1]}/>
-    <VideoListEntry video={props.videos[2]}/>
-
+      {props.videos.map(video =>
+        <VideoListEntry video={video} />
+      )}
   </div>
 );
 
