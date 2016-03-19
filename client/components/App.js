f4209e5c9cf89dd1f7f5fce28a85c701d13ac68d
David Ogor <doctown@yahoo.com>
2016-03-18 20:43:14 -0700

    Make App a class

--- client/components/App.js
+++ client/components/App.js
@@ -1,13 +1,20 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList videos={exampleVideoData}/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer/>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={exampleVideoData}/>
+        </div>
+      </div>
+    );
+  }
+}
 ReactDOM.render(<App />, document.getElementById("app"));
--- client/components/VideoListEntry.js
+++ client/components/VideoListEntry.js
@@ -1,21 +1,13 @@
-class VideoListEntry extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
+var VideoListEntry = (props) => (
       <div className="video-list-entry">
         <div className="media-left media-middle">
-          <img className="media-object" src={this.props.video.snippet.thumbnails.default.url} alt=""/>
+          <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt=""/>
         </div>
         <div className="media-body">
-          <div className="video-list-entry-title">{this.props.video.snippet.title}</div>
-          <div className="video-list-entry-detail">{this.props.video.snippet.description}</div>
+          <div className="video-list-entry-title">{props.video.snippet.title}</div>
+          <div className="video-list-entry-detail">{props.video.snippet.description}</div>
         </div>
       </div>
     );
-  }
-}
 
 window.VideoListEntry = VideoListEntry;
