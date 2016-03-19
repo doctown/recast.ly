f25516391ee53d7b21695e796f57f5c18cd44448
David Ogor <doctown@yahoo.com>
2016-03-18 20:51:51 -0700

    Make Video work

--- client/components/App.js
+++ client/components/App.js
@@ -1,6 +1,10 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = ({
+      'currentVideo': exampleVideoData[0],
+      'videoList': exampleVideoData
+    });
   }
 
   render() {
@@ -8,10 +12,10 @@ class App extends React.Component {
       <div>
         <Nav />
         <div className="col-md-7">
-          <VideoPlayer/>
+          <VideoPlayer video={this.state.currentVideo}/>
         </div>
         <div className="col-md-5">
-          <VideoList videos={exampleVideoData}/>
+          <VideoList videos={this.state.videoList}/>
         </div>
       </div>
     );
